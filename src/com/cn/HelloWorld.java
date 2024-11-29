43ca6cad14393cc0b6b52ee589319139690b4480
liuchuang <liuchuang@qq.com>
2024-11-29 16:41:28 +0800

    zs 创建dev分支进行第一提交

--- src/com/cn/HelloWorld.java
+++ src/com/cn/HelloWorld.java
@@ -3,5 +3,6 @@ package com.cn;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("开发人员张三在dev分支开发新功能了");
     }
 }
