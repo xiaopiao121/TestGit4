a7a186f8521cc2ddb57c6b7c745c67cb41a30cbb
liuchuang <liuchuang@qq.com>
2024-11-29 15:47:08 +0800

    lc 修改Task.java类编写内容。

--- src/com/cn/Task.java
+++ src/com/cn/Task.java
@@ -3,8 +3,8 @@ package com.cn;
 public class Task {
     public static void main(String[] args) {
         System.out.println("这是一个Task类");
-        System.out.println("编写任务内容");
         System.out.println("任务一。。。");
+        System.out.println("编写任务内容");
 
     }
 }
